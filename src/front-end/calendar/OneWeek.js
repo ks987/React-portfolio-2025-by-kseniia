09142da28c5812ce38125c9e83cb808a183e48a8
Kseniia Bondarenko <ksen9291@gmail.com>
2025-01-23 15:50:14 -0800

    Progress-3:50pm-Jan-23-2025

--- src/front-end/calendar/OneWeek.js
+++ src/front-end/calendar/OneWeek.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 
-
+// import navbar and footer
 import CalendarNavbar from './calendar-navbar/CalendarNavbar.js';
-
+import Footer from '../Footer.js';
 
 import './OneWeek.css';
 
 
 
 export default function OneWeek() {
+
+
+    function handleChange() {
+
+
+    }
+
+    function sayTime() {
+
+    }
+    
+
 
 
     return (
@@ -23,30 +35,362 @@ export default function OneWeek() {
                 <button><i class="fa-solid fa-arrow-right"></i></button>
             </div>
             <br></br>
-            <table>
-                <tr>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                    <th>Sunday</th>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
 
-                    <td></td>
-                    <td></td>
-                    <td></td>
+            <div className="OneWeek-table">
+                <div className="OneWeek-table-row OneWeek-table-row-1">
+                    <div className="OneWeek-table-time"></div>
+                    <div className="OneWeek-day-of-week">Monday</div>
 
-                </tr>
+                    <div className="OneWeek-day-of-week">Tuesday</div>
+                    <div className="OneWeek-day-of-week">Wednesday</div>
+                    <div className="OneWeek-day-of-week">Thursday</div>
 
-            </table>
+                    <div className="OneWeek-day-of-week">Friday</div>
+                    <div className="OneWeek-day-of-week">Saturday</div>
+                    <div className="OneWeek-day-of-week">Sunday</div>
+                </div>
 
+                <div className="OneWeek-table-row OneWeek-table-row-2">
+                    <div className="OneWeek-table-time"><div className="OneWeek-time-note">12:00am</div></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-3">
+                    <div className="OneWeek-table-time">1:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-4">
+                    <div className="OneWeek-table-time">2:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-5">
+                    <div className="OneWeek-table-time">3:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-6">
+                    <div className="OneWeek-table-time">4:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-7">
+                    <div className="OneWeek-table-time">5:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-8">
+                    <div className="OneWeek-table-time">6:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-9">
+                    <div className="OneWeek-table-time">7:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-10">
+                    <div className="OneWeek-table-time">8:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-11">
+                    <div className="OneWeek-table-time">9:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-12">
+                    <div className="OneWeek-table-time">10:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-13">
+                    <div className="OneWeek-table-time">11:00 AM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">12:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">1:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">2:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">3:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">4:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">5:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">6:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">7:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">8:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">9:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">10:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+                <div className="OneWeek-table-row OneWeek-table-row-14">
+                    <div className="OneWeek-table-time">11:00 PM </div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                    <div className="OneWeek-timeslot"></div>
+                </div>
+
+
+
+
+
+
+
+            </div>
+
+<br></br>
+<br></br>
+<br></br>
+        <Footer/>
 
         </div>
     )
