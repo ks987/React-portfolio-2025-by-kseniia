5a5379c2f2b9fbda20f787efdaf84dc8b9409eeb
Kseniia Bondarenko <ksen9291@gmail.com>
2025-02-01 19:26:37 -0800

    progress-7.26pm-Feb-1-2025

--- src/front-end/calendar/Sidebar.js
+++ src/front-end/calendar/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import PickCalendarColor from './PickCalendarColor.js';
-
+import AddNewCalendar from './Add-new-calendar.js';
 import './Sidebar.css';
 
 export default function Sidebar() {
@@ -14,22 +14,78 @@ export default function Sidebar() {
     const month = today.getMonth();
     const year = today.getFullYear();
 
-    const [date, setDate] = useState('');
-    const [circleColor, setCircleColor] = useState('#ffddcc');
+    const calendarNames = ['Main Calendar', 'Sample Schedule', 'Holidays', 'Travel Calendar', 'Menu Calendar'];
 
+
+    const [circleColor, setCircleColor] = useState('#ffbb11');
+    const [isVisible, setIsVisible] = useState(true);
+    const [checkedItems, setCheckedItems] = useState({ calendarNames }); // track checked conditions for every calendar
+    const [checked, setChecked] = useState('fa-solid fa-square-check');
+    const [addNewCalendarOn, setAddNewCalendarOn] = useState(true);
+
+    // determine day of week
     function getWeekday() {
         return weekDayArray[new Date().getDay()];
     }
 
+    // set new color 
     function handleColorChange(newColor) {
         setCircleColor(newColor);
     }
 
 
+    // show or hide the color picker
+
+    function toggleColorPicker() {
+        setIsVisible(!isVisible);
+    }
+
+
+
+    function toggleAllCheckboxes() {
+        if(checked === 'fa-solid fa-square-check') {
+            setChecked('fa-solid fa-square');
+        } else {
+            setChecked('fa-solid fa-square-check');
+        }
+    }
+
+    function toggleOneCheckbox(index) {
+
+        setCheckedItems(checkedItems.map(() => { 
+            if (checked === 'fa-solid fa-square-check') {
+                calendarNames[index].setChecked('fa-solid fa-square');
+            } else {
+                calendarNames[index].setChecked('fa-solid fa-square-check');
+            }
+
+        }))
+   
+    }
+
+
+
+    // set custom ID to each checkbox icon
+
+    const [iconID, setIconID] = useState('checkbox-1');
+
+
+
+    // check or uncheck the custom checkbox 
+
+
+// display a field to add a new calendar to the list
+
+function displayAddNewCalendar() {
+    setAddNewCalendarOn(!addNewCalendarOn);
+}
+
     return (
         <div className="Sidebar">
 
-            <PickCalendarColor onColorChange={handleColorChange}/>
+            <div className="Sidebar-color-picker-box">
+                {!isVisible && <PickCalendarColor onColorChange={handleColorChange} />}
+            </div>
 
             <div className="Sidebar-today">
                 <div>Today is {getWeekday()}, </div>
@@ -38,41 +94,29 @@ export default function Sidebar() {
 
 
 
-            <div className="CalendarsList-list">
+            <div className="Sidebar-list">
                 <div className="Sidebar-title">My Calendars: </div>
                 <div className="calendars-list-itself">
 
-                    <div className="Sidebar-row">
-                        <input type="checkbox"></input>
-                        Main Calendar
-                        <div style={{background: circleColor}} className="Sidebar-color-circle"></div>
-                    </div>
+                    {
+                        calendarNames.map((title, index) => (
+                            <div key={index} className="Sidebar-row" id={`row-${index + 1}`}>
+                                <input type="checkbox" style={{ display: "none" }}></input>
+                                <i className={`${checked} Sidebar-custom-checkbox`}
+                                    onClick={() => (toggleAllCheckboxes(index))}></i>
+                                {title}, {index}
+                                <div style={{ background: circleColor }} className="Sidebar-color-circle"></div>
+                            </div>
+                        ))
+                    }
 
-                    <div className="Sidebar-row">
-                        <input type="checkbox"></input>
-                        Workday Simulation
-                        <div className="Sidebar-color-circle"></div>
-                    </div>
+                </div>
 
-                    <div className="Sidebar-row">
-                        <input type="checkbox"></input>
-                        Sample Schedule
-                        <div className="Sidebar-color-circle"></div>
-                    </div>
+                <div>
+                    <button className="Sidebar-add-btn"
+                    onClick={displayAddNewCalendar}>Add new calendar</button>
 
-                    <div className="Sidebar-row">
-                        <input type="checkbox"></input>
-                        Travel Planning
-                        <div className="Sidebar-color-circle"></div>
-                    </div>
-
-                    <div className="Sidebar-row">
-                        <input type="checkbox"></input>
-                        Holidays
-                        <div className="Sidebar-color-circle"></div>
-                    </div>
-
-
+                    {!addNewCalendarOn && <AddNewCalendar/>}
                 </div>
             </div>
         </div>
