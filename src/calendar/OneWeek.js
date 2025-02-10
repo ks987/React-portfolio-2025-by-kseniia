de308e8edb0daeea3c9af816f30c06cf4be4e763
Kseniia Bondarenko <ksen9291@gmail.com>
2025-02-09 19:55:43 -0800

    progress-7.55pm-Feb-9-2025

--- src/calendar/OneWeek.js
+++ src/calendar/OneWeek.js
@@ -9,8 +9,7 @@ import Footer from '../Footer.js';
 
 // import other functions
 import Overlay from './Overlay.js';
-import FrontEndDatabase from './Front-end-database.js';
-
+import './database-sample.js';
 
 
 import './OneWeek.css';
@@ -18,7 +17,7 @@ import './OneWeek.css';
 
 
 export default function OneWeek() {
-
+    // front-end database with tasks 
 
     const [isVisible, setIsVisible] = useState(false);
     const [rowColor, setRowColor] = 'gold';
@@ -26,7 +25,6 @@ export default function OneWeek() {
     const toggleOverlay = () => {
         setIsVisible(!isVisible);
     }
-
 
 
     function handleChange() {
@@ -38,10 +36,46 @@ export default function OneWeek() {
 
     }
 
+    const oldTimesOfDay = ['12:00 AM', '1:00 AM', '2:00 AM', '3:00 AM', '4:00 AM', '5:00 AM', '6:00 AM', '7:00 AM'];
 
-    const timesOfDay = ['12:00 AM', '1:00 AM', '2:00 AM', '3:00 AM', '4:00 AM', '5:00 AM', '6:00 AM', '7:00 AM'];
-    const namesOfDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    const timesOfDay = ['12:00 AM', '1:00 AM', '2:00 AM', '3:00 AM', '4:00 AM', '5:00 AM', '6:00 AM',
+        '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM',
+        '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM', '11:00 PM'];
+
+
+    const namesOfDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     const emptySlots = ['', '', '', '', '', '', ''];
+    const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    const [monthStart, setMonthStart] = useState('Feb');
+    const [monthEnd, setMonthEnd] = useState('Feb');
+    const [dayStart, setDayStart] = useState('5');
+    const [dayEnd, setDayEnd] = useState('11');
+    const [yearEnd, setYearEnd] = useState('2024');
+
+
+
+
+    function addOneWeek() {
+        setMonthStart('Feb');
+        setMonthEnd('Feb');
+        setDayStart('12');
+        setDayEnd('18');
+        setYearEnd('2024');
+
+
+
+    }
+
+
+    function subtractOneWeek() {
+        setMonthStart('Jan');
+        setMonthEnd('Jan');
+        setDayStart('50');
+        setDayEnd('51');
+        setYearEnd('1900');
+    }
+
 
     return (
         <div className="OneWeek">
@@ -52,65 +86,46 @@ export default function OneWeek() {
             <br></br>
 
             <div className="OneWeek-top-row">
-                <button><i class="fa-solid fa-arrow-left"></i></button>
-                <div className="month-year-label">January 20-26, 2025</div>
-                <button><i class="fa-solid fa-arrow-right"></i></button>
+                <button onClick={subtractOneWeek}><i class="fa-solid fa-arrow-left"></i></button>
+                <div className="month-year-label">{monthStart} {dayStart} - {monthEnd} {dayEnd}, {yearEnd}</div>
+                <button onClick={addOneWeek}><i class="fa-solid fa-arrow-right"></i></button>
             </div>
             <br></br>
 
 
-            <FrontEndDatabase/>
-
             <br></br>
-
-
 
             <div className="OneWeek-table">
 
 
-                <div className="OneWeek-table-row OneWeek-table-row-1">
+                <div className="OneWeek-table-row-1">
                     <div className="OneWeek-table-time"></div>
-                    <div className="OneWeek-day-of-week">Monday</div>
+                    {namesOfDays.map((dayOfWeek) => (
+                        <div>
+                            <div className="OneWeek-day-of-week">{dayOfWeek}</div>
+                        </div>
 
-                    <div className="OneWeek-day-of-week">Tuesday</div>
-                    <div className="OneWeek-day-of-week">Wednesday</div>
-                    <div className="OneWeek-day-of-week">Thursday</div>
+                    ))}
 
-                    <div className="OneWeek-day-of-week">Friday</div>
-                    <div className="OneWeek-day-of-week">Saturday</div>
-                    <div className="OneWeek-day-of-week">Sunday</div>
                 </div>
 
 
-                <div>
-                    {timesOfDay.map((time, rowIndex) => {
+                <div className="OneWeek-all-other-rows">
+                    {timesOfDay.map((time, rowIndex) => (
                         <div key={rowIndex} className="OneWeek-table-row">
                             <div className="OneWeek-table-time">{time}</div>
 
-                            {emptySlots.map((weekday, indexOfDay) => {
-                                <div key={indexOfDay} className="OneWeek-timeslot" onClick={toggleOverlay}>{weekday}</div>
-                            })}
+
+                            {
+                                emptySlots.map((weekday, indexOfDay) => (
+                                    <div key={indexOfDay} className="OneWeek-timeslot" onClick={toggleOverlay}>{weekday}</div>
+                                ))
+                            }
 
                         </div>
-
-
-
-                    })}
+                    ))}
 
                 </div>
-
-
-                {/* <div className="OneWeek-table-row OneWeek-table-row-4">
-                    <div className="OneWeek-table-time">2:00 AM </div>
-                    <div className="OneWeek-timeslot" onClick={toggleOverlay}></div>
-                    <div className="OneWeek-timeslot" onClick={toggleOverlay}></div>
-                    <div className="OneWeek-timeslot" onClick={toggleOverlay}></div>
-                    <div className="OneWeek-timeslot" onClick={toggleOverlay}></div>
-
-                    <div className="OneWeek-timeslot" onClick={toggleOverlay}></div>
-                    <div className="OneWeek-timeslot" onClick={toggleOverlay}></div>
-                    <div className="OneWeek-timeslot" onClick={toggleOverlay}></div>
-                </div> */}
 
 
 
@@ -120,6 +135,8 @@ export default function OneWeek() {
             <br></br>
             <br></br>
             <Footer />
+
+
 
         </div>
 
---
+++ src/calendar/database-sample.js
@@ -0,0 +1,61 @@
+const database1 = [
+    {
+        'date': 'February 3, 2024',
+        'dayOfWeek': 'Saturday',
+        'time': '10:00 AM - 11:30 AM',
+        'taskTitle': 'Go grocery shopping',
+        'taskDescription': {
+            'descriptionType': 'checkboxes',
+            'descriptionContents': [
+                '2 containers of tomatoes',
+                '1 container of spinach',
+                'cauliflower',
+                '2 bags of potatoes',
+                'oats',
+                'whole wheat bread',
+                'sourdough bread',
+                '4 avocadoes',
+                'blueberries',
+                'bananas',
+                'apples',
+                'raisins',
+                'medjool dates',
+                'pumpkin seeds',
+                'almond butter',
+                'dried cayenne pepper']
+        }
+    },
+
+    {
+        'date': 'February 4, 2024',
+        'dayOfWeek': 'Sunday',
+        'time': '3:00 PM - 8:00 PM',
+        'taskTitle': 'Meet with friends',
+    },
+
+    {
+        'date': 'February 5, 2024',
+        'dayOfWeek': 'Monday',
+        'time': '8:00 AM - 9:00 AM',
+        'taskTitle': 'Zoom meeting',
+
+    },
+
+    {
+        'date': 'February 6, 2024',
+        'dayOfWeek': 'Tuesday',
+        'time': '11:00 AM - 12:00 PM',
+        'taskTitle': 'Dental cleaning appt.',
+    },
+
+
+    {
+        'date': 'February 7, 2024',
+        'dayOfWeek': 'Wednesday',
+        'time': '6:00 PM - 10:00 PM',
+        'taskTitle': 'Bake cookies',
+
+    }
+
+
+];
