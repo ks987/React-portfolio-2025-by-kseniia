3efc71918addf8231b735a57f1231a31c5a3838e
Kseniia Bondarenko <ksen9291@gmail.com>
2025-02-11 09:09:34 -0800

    progress-9.09am-Feb-11-2025

--- src/calendar/OneMonth.js
+++ src/calendar/OneMonth.js
@@ -1,12 +1,14 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { tasksInDatabase } from './database-sample.js';
 
 
 // import js files
 import CalendarNavbar from './calendar-navbar/CalendarNavbar.js';
 import Overlay from './Overlay.js';
 import Sidebar from './Sidebar.js';
-import Footer from  '../Footer.js';
+import Footer from '../Footer.js';
+
+
 
 // import css files
 import './OneMonth.css';
@@ -16,27 +18,26 @@ import './OneMonth.css';
 export default function OneMonth() {
 
     // toggle visiblity of overlay with assigning tasks
-
     const [isVisible, setIsVisible] = useState(false);
+    // toggle the pop-up showing one assigned task 
+    const [oneTaskIsVisible, setOneTaskIsVisible] = useState(false);
 
-    const toggleOverlay = () => {
-        setIsVisible(!isVisible);
-    }
 
+    // toggle the pop-up showing all assigned tasks
+    const [oneDayTasksAreVisible, setOneDayTasksAreVisible] = useState(false);
+
+
+    const [numberOfTasks, setNumberOfTasks] = useState('');
 
     // render a month-long calendar
-
     const daysInMonth = Array(49).fill(null); // Maximum slots in a calendar (6 weeks)
 
-
     const [dayOfMonth, setDayOfMonth] = useState('');
-
 
     // all months in existence
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
     // determine today's date
-
     const today = new Date();
     const day = today.getDate(); // day of the month
     const month = today.getMonth() + 1; // Month is indexed at 0, so add 1
@@ -47,14 +48,20 @@ export default function OneMonth() {
     const wordMonth = `${months[month - 1]}`;
     const onlyYear = `${year}`;
 
-
-
-
-    // add one more month to the date
     const [nextMonth, setNextMonth] = useState(wordMonth);
     const [nextYear, setNextYear] = useState(onlyYear);
 
 
+
+    // toggle visiblity of overlay with assigning tasks
+
+    const toggleOverlay = () => {
+        setIsVisible(!isVisible);
+    }
+
+
+
+    // add one more month to the date
     const addOneMonth = () => {
         const currMonthIndex = months.indexOf(nextMonth);
         if (currMonthIndex === 11) {
@@ -79,12 +86,61 @@ export default function OneMonth() {
     }
 
 
+    // determine number of tasks in existence per one day
+
+
+    const determineNumberOfTasks = () => {
+
+        setNumberOfTasks(`${tasksInDatabase.length} more tasks`);
+
+
+        if (numberOfTasks === '') {
+            setNumberOfTasks('');
+        } else if (numberOfTasks === NaN) {
+            setNumberOfTasks('');
+        } else if (numberOfTasks === 1) {
+            setNumberOfTasks(`${tasksInDatabase.length} more task`);
+        } else if (numberOfTasks === 0) {
+            setNumberOfTasks('');
+        } else {
+            setNumberOfTasks(`${tasksInDatabase.length} more tasks`);
+        }
+
+
+    }
+
+
+
+    useEffect(() => {
+        determineNumberOfTasks();
+    }, []);
+
+
+
+    // display a pop-up with all the tasks assigned for that day
+
+
 
     return (
         <div className="OneMonth">
             <CalendarNavbar />
 
             {isVisible && <Overlay />}
+
+
+            {!oneDayTasksAreVisible && (<div className="OneMonth-all-tasks-popup">
+                <div>
+                    <div>read</div>
+                    <div>take a nap</div>
+                    <div>go on a walk</div>
+                    <div>learn French</div>
+                    <div>code</div>
+                </div>
+            </div>)}
+
+            {!oneTaskIsVisible && (<div className="OneMonth-one-task-popup">
+                    <div>6:00 am read Kindle</div>
+            </div>)}
 
             <br></br>
 
@@ -100,22 +156,37 @@ export default function OneMonth() {
 
 
             <div className="OneMonth-sidebar-and-one-month">
-                <Sidebar/>
+                <Sidebar />
                 <div>
-                <button className="SampleMonth-add-new-task-btn"
-                    onClick={toggleOverlay}>Add New Task</button>
+                    <button className="SampleMonth-add-new-task-btn"
+                        onClick={toggleOverlay}>Add New Task</button>
 
 
 
 
-                <div className="OneMonth-monthly-calendar">
+                    <div className="OneMonth-monthly-calendar">
 
-                    {daysInMonth.map((dayOfMonth, index) => (
-                        <div key={index} className="OneMonth-slot">{dayOfMonth}</div>
-                    ))}
+                        {daysInMonth.map((_, index) => (
+                            <div key={index} className="OneMonth-slot">
+
+                                {index}
+                                <div className="OneMonth-inserted-tasks">
+                                    <div className="OneMonth-inserted-task-row">
+                                        <div className="OneMonth-inserted-time">all day</div>
+                                        <div className="OneMonth-inserted-task"> Code</div>
+                                    </div>
+
+                                    <div className="OneMonth-number-of-tasks">
+                                        {numberOfTasks}
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        ))}
 
 
-                </div>
+                    </div>
                 </div>
 
 
@@ -123,9 +194,8 @@ export default function OneMonth() {
             </div>
 
             {/* sample overlay */}
-            <Overlay/>
 
-            <Footer/>
+            <Footer />
         </div>
     )
 }
--- src/calendar/database-sample.js
+++ src/calendar/database-sample.js
@@ -1,8 +1,9 @@
-const database1 = [
+export const tasksInDatabase = [
     {
         'date': 'February 3, 2024',
         'dayOfWeek': 'Saturday',
         'time': '10:00 AM - 11:30 AM',
+        'calendarTitle': 'Main Calendar',
         'taskTitle': 'Go grocery shopping',
         'taskDescription': {
             'descriptionType': 'checkboxes',
@@ -30,6 +31,7 @@ const database1 = [
         'date': 'February 4, 2024',
         'dayOfWeek': 'Sunday',
         'time': '3:00 PM - 8:00 PM',
+        'calendarTitle': 'Main Calendar',
         'taskTitle': 'Meet with friends',
     },
 
@@ -37,6 +39,7 @@ const database1 = [
         'date': 'February 5, 2024',
         'dayOfWeek': 'Monday',
         'time': '8:00 AM - 9:00 AM',
+        'calendarTitle': 'Main Calendar',
         'taskTitle': 'Zoom meeting',
 
     },
@@ -45,6 +48,7 @@ const database1 = [
         'date': 'February 6, 2024',
         'dayOfWeek': 'Tuesday',
         'time': '11:00 AM - 12:00 PM',
+        'calendarTitle': 'Main Calendar',
         'taskTitle': 'Dental cleaning appt.',
     },
 
@@ -53,9 +57,58 @@ const database1 = [
         'date': 'February 7, 2024',
         'dayOfWeek': 'Wednesday',
         'time': '6:00 PM - 10:00 PM',
+        'calendarTitle': 'Main Calendar',
         'taskTitle': 'Bake cookies',
 
+    }, 
+    // 2025 tasks
+    {
+        'date': 'February 11, 2025',
+        'dayOfWeek': 'Tuesday',
+        'time': '6:00 AM - 6:20 AM',
+        'calendarTitle': 'Main Calendar',
+        'taskTitle': 'Green tea',
+
+    },
+
+    {
+        'date': 'February 11, 2025',
+        'dayOfWeek': 'Tuesday',
+        'time': '7:00 AM - 8:30 AM',
+        'calendarTitle': 'Main Calendar',
+        'taskTitle': 'Go on a walk',
+
+    },
+
+    {
+        'date': 'February 11, 2025',
+        'dayOfWeek': 'Tuesday',
+        'time': '9:00 AM - 5:00 PM',
+        'calendarTitle': 'Main Calendar',
+        'taskTitle': 'Code',
+
+    },
+
+    {
+        'date': 'February 11, 2025',
+        'dayOfWeek': 'Tuesday',
+        'time': '5:00 PM - 6:00 PM',
+        'calendarTitle': 'Main Calendar',
+        'taskTitle': 'Dinner',
+
+    },
+
+
+    {
+        'date': 'February 11, 2025',
+        'dayOfWeek': 'Tuesday',
+        'time': '7:00 PM - 9:00 PM',
+        'calendarTitle': 'Main Calendar',
+        'taskTitle': 'Read Kindle',
+
     }
+
+
 
 
 ];
@@ -63,3 +116,5 @@ const database1 = [
 
 
 const calendarDatabase = ['Main Calendar', 'Sample Schedule', 'Holidays', 'Travel Calendar', 'Menu Calendar'];
+
+
